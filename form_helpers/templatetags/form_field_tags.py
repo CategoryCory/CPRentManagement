3ee0991a5824a7584e88afe5899e6ec403553d4a
Cory Coward <cory@c2softwaresolutions.com>
2022-06-13 07:53:13 -0500

    Update unit, proeprty, and company forms to use form field tags

--- form_helpers/templatetags/form_field_tags.py
+++ form_helpers/templatetags/form_field_tags.py
@@ -3,9 +3,14 @@ from django import template
 register = template.Library()
 
 
-@register.inclusion_tag('form_field.html')
-def form_field_template(field, num_cols):
+@register.inclusion_tag('form_input.html')
+def form_input(field, num_cols):
     return {'field': field, 'num_cols': num_cols}
+
+
+@register.inclusion_tag('form_checkbox.html')
+def form_checkbox(field):
+    return {'field': field}
 
 
 @register.inclusion_tag('form_button_row.html')
